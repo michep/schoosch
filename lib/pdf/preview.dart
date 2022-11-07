27b436371994524fc2db76958e4a4815a81813de
michep <>
2022-11-07 15:48:41 +0300

    initial implementation of pdf

---
+++ lib/pdf/preview.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:printing/printing.dart';
+
+class Preview extends StatelessWidget {
+  final LayoutCallback generate;
+  final PdfPageFormat format;
+
+  const Preview({
+    required this.generate,
+    required this.format,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PdfPreview(
+        initialPageFormat: format,
+        build: (PdfPageFormat format) {
+          return generate(format);
+        },
+      ),
+    );
+  }
+}
