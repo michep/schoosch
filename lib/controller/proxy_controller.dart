c16cac37304e92a2d1ed934d1e84f34cd62d265a
michep <>
2022-12-18 02:48:46 +0300

    next step froward

--- lib/controller/proxy_controller.dart
+++ lib/controller/proxy_controller.dart
@@ -664,9 +664,7 @@ class ProxyStore extends getx.GetxController {
   }
 
   Future<List<PeriodMarkModel>> getStudentAllPerioddMarks(StudentModel student, List<CurriculumModel> curriculums) async {
-    // var year = await currentInstitution!.currentYearPeriod;
-    var periods = await currentInstitution!.currentYearSemesterPeriods;
-    periods.add((await currentInstitution!.currentYearPeriod)!);
+    var periods = await currentInstitution!.currentYearAndSemestersPeriods;
     var res = await dio.postUri<List>(
       baseUriFunc('/student/${student.id}/curriculums/mark/periods'),
       options: Options(headers: {'Content-Type': 'application/json'}),
@@ -678,15 +676,6 @@ class ProxyStore extends getx.GetxController {
     var js = res.data!;
     return js.map((e) => PeriodMarkModel.fromMap(e['_id'], e)).toList();
   }
-
-  //637102cdc77fccc0ea9d0324
-  //6378fe0f836d36dc1d7b68e7
-
-  //637100c9ae30991818734d1e institution
-
-  //637d44cc6586e2b97e585f94 1
-  //637d44e06586e2b97e585f95 2
-  //637d44ad6586e2b97e585f93 year
 
   Future<String> saveLessonMark(LessonMarkModel mark) async {
     var data = mark.toMap(withId: true);
