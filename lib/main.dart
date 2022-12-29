9964c30b741ec69cfb512900e76fea83085d6e3e
michep <>
2022-12-29 14:36:37 +0300

    fix api host

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   var fauth = FAuth();
-  // var proxy = ProxyStore((path) => Uri.https('www.chepaykin.org:8182', path));
-  var proxy = ProxyStore((path) => Uri.http('localhost:8182', path));
+  var proxy = ProxyStore((path) => Uri.https('www.chepaykin.org:8182', path));
+  // var proxy = ProxyStore((path) => Uri.http('localhost:8182', path));
   var curweek = CurrentWeek(Week.current());
   var prefs = PrefsController();
   await prefs.init();
