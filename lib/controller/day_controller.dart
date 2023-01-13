7c7614fc65304ebe722e0b276dad393940e84b83
michep <>
2023-01-14 01:19:48 +0300

    fix nav across new year, happy new year, may we all have a visi .... sorry

--- lib/controller/day_controller.dart
+++ lib/controller/day_controller.dart
@@ -15,25 +15,33 @@ class CurrentDay extends GetxController {
   CurrentDay(DateTime dt) {
     _currentDay.value = dt;
     _pageController = PageController(
-      initialPage: currentDay.year * 1000 +
-          int.parse(
-            DateFormat('D').format(
-              currentDay,
-            ),
-          ),
+      initialPage: currentDay.year * 1000 + int.parse(DateFormat('D').format(currentDay)),
     );
   }
 
   void setIdx(int idx) {
+    var idxYear = idx ~/ 1000;
+    var idxDay = idx % 1000;
+    var maxDay = int.parse(DateFormat('D').format(DateTime(idxYear, 12, 31)));
+
+    if (idxDay == 0) {
+      _currentDay.value = DateTime(idxYear - 1, 12, 31);
+      var nIdx = (currentDay.year) * 1000 + int.parse(DateFormat('D').format(currentDay));
+      if (_pageController.hasClients) _pageController.jumpToPage(nIdx);
+      idx = nIdx;
+      return;
+    } else if (idxDay == maxDay + 1) {
+      _currentDay.value = DateTime(idxYear + 1, 1, 1);
+      var nIdx = (currentDay.year) * 1000 + int.parse(DateFormat('D').format(currentDay));
+      if (_pageController.hasClients) _pageController.jumpToPage(nIdx);
+      idx = nIdx;
+      return;
+    }
+
     _currentDay.value = DateFormat('y D').parse('${idx ~/ 1000} ${idx % 1000}');
     if (_pageController.hasClients) {
       _pageController.animateToPage(
-        currentDay.year * 1000 +
-            int.parse(
-              DateFormat('D').format(
-                currentDay,
-              ),
-            ),
+        currentDay.year * 1000 + int.parse(DateFormat('D').format(currentDay)),
         duration: const Duration(milliseconds: 1000),
         curve: Curves.easeOutExpo,
       );
@@ -44,11 +52,7 @@ class CurrentDay extends GetxController {
     if (_currentDay.value.weekday == 7) {
       Get.find<CurrentWeek>().next();
     } else {
-      _currentDay.value = _currentDay.value.add(
-        const Duration(
-          days: 1,
-        ),
-      );
+      // _currentDay.value = _currentDay.value.add(const Duration(days: 1));
       if (_pageController.hasClients) {
         _pageController.nextPage(
           duration: const Duration(milliseconds: 1000),
@@ -60,15 +64,9 @@ class CurrentDay extends GetxController {
 
   void previous() {
     if (_currentDay.value.weekday == 1) {
-      Get.find<CurrentWeek>().previous(
-        isFromDay: true,
-      );
+      Get.find<CurrentWeek>().previous(isFromDay: true);
     } else {
-      _currentDay.value = _currentDay.value.subtract(
-        const Duration(
-          days: 1,
-        ),
-      );
+      // _currentDay.value = _currentDay.value.subtract(const Duration(days: 1));
       if (_pageController.hasClients) {
         _pageController.previousPage(
           duration: const Duration(milliseconds: 1000),
@@ -83,12 +81,7 @@ class CurrentDay extends GetxController {
     if (!isFromWeek) Get.find<CurrentWeek>().setIdx(date.year * 100 + Week.fromDate(date).weekNumber);
     if (_pageController.hasClients) {
       _pageController.animateToPage(
-        date.year * 1000 +
-            int.parse(
-              DateFormat('D').format(
-                date,
-              ),
-            ),
+        date.year * 1000 + int.parse(DateFormat('D').format(date)),
         duration: const Duration(milliseconds: 1000),
         curve: Curves.easeOutExpo,
       );
@@ -97,18 +90,8 @@ class CurrentDay extends GetxController {
 
   Future<void> changeToCurrentDay() async {
     var date = DateTime.now();
-    var nIdx = date.year * 1000 +
-        int.parse(
-          DateFormat('D').format(
-            date,
-          ),
-        );
-    var cIdx = currentDay.year * 1000 +
-        int.parse(
-          DateFormat('D').format(
-            currentDay,
-          ),
-        );
+    var nIdx = date.year * 1000 + int.parse(DateFormat('D').format(date));
+    var cIdx = currentDay.year * 1000 + int.parse(DateFormat('D').format(currentDay));
     int direction = 0;
     if (nIdx > cIdx) direction = -1;
     if (nIdx < cIdx) direction = 1;
