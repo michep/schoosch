49241e7a2ca1e00d9aa03a13bb9de186aa5665f7
michep <michep@mail.ru>
2025-12-08 10:23:46 +0300

    firebase plugin removed from gradle

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // var proxy = ProxyStore((path) => Uri.https('www.chepaykin.org', '/schoosch/api$path')); //real
   var proxy = ProxyStore((path) => Uri.http('localhost:8182', '/schoosch/api$path')); // local
+  // var proxy = ProxyStore((path) => Uri.http('10.0.2.2:8182', '/schoosch/api$path')); // local emulator
   var curweek = CurrentWeek(Week.current());
   var prefs = PrefsController();
   await prefs.init();
