e691809ba70ae60d325d6f7da4421bd8d5dcafcf
michep <michep@mail.ru>
2025-12-28 20:36:35 +0300

    v2 api host

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,9 @@ import 'package:schoosch/theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  var proxy = ProxyStore((path) => Uri.https('www.chepaykin.org', '/schoosch/v2/api$path')); //new real api
   // var proxy = ProxyStore((path) => Uri.https('www.chepaykin.org', '/schoosch/api$path')); //real
-  var proxy = ProxyStore((path) => Uri.http('localhost:8182', '/schoosch/api$path')); // local
+  // var proxy = ProxyStore((path) => Uri.http('localhost:8182', '/schoosch/api$path')); // local
   // var proxy = ProxyStore((path) => Uri.http('10.0.2.2:8182', '/schoosch/api$path')); // local emulator
   var curweek = CurrentWeek(Week.current());
   var prefs = PrefsController();
